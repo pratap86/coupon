974e50a2ba95fb7e009f52ab63fc1cba1d77da5d
pratap86 <narayanpratap86@gmail.com>
2021-07-25 17:12:59 +0530

    initial commit

---
+++ src/test/java/com/pratap/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratap.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
